fb4b9ce229ca1e419878ec4f21ec9ca1e4410244
AgustinZ92 <165961945+AgustinZ92@users.noreply.github.com>
2024-06-26 13:54:03 -0300

    V1.1

--- apireest/src/main/java/com/example/apireest/controllers/controller.java
+++ apireest/src/main/java/com/example/apireest/controllers/controller.java
@@ -3,7 +3,6 @@ package com.example.apireest.controllers;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.apireest.modelo.auto;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -22,15 +21,19 @@ public class controller {
     public List<auto> getAutos() {
         auto[] autos = new auto[3];
 
-        autos[0] = new auto("Toyota", "Corolla", 2020);
-        autos[1] = new auto("Honda", "Civic", 2019);
-        autos[2] = new auto("Ford", "Focus", 2018);
+        autos[0] = new auto("Toyota", "Corolla", 2019);
+        autos[1] = new auto("Honda", "Civic", 2017);
+        autos[2] = new auto("Ford", "Focus", 2014);
 
         List<auto> listaAutos = new ArrayList<>(Arrays.asList(autos));
 
         listaAutos.forEach(auto -> {
-            auto.setMarca("test");
-            auto.setAno(auto.getAno() + 7);
+            String marca = auto.getMarca();
+            auto.setAno(auto.getAno());
+            if ("Toyota".equals(marca) || "Honda".equals(marca) || "Ford".equals(marca)) {
+            } else {
+                auto.setMarca("Desconocida");
+            }
         });
 
         return List.of(autos);
